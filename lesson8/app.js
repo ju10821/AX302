e6947412c9cb9d72461cc99b809c8c6d1061322f
juliettedeiss <juliettedeiss@gmail.com>
2019-02-27 17:47:09 +0800

    update

--- lesson8/app.js
+++ lesson8/app.js
@@ -8,7 +8,7 @@ function preload(){
 	game.load.image('ground', 'assets/platform.png');
 	game.load.image('star', 'assets/star.png');
 	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-	game.load.spritesheet('baddie', 'assets/baddie.png', 32);
+	game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
 }
 
 function create(){
