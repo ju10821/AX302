3737d5aa85896c9f688c9263025e8728257ed9f0
juliettedeiss <juliettedeiss@gmail.com>
2019-02-27 17:32:20 +0800

    update

--- lesson8/app.js
+++ lesson8/app.js
@@ -8,7 +8,7 @@ function preload(){
 	game.load.image('ground', 'assets/platform.png');
 	game.load.image('star', 'assets/star.png');
 	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-	game.load.spritesheet('baddie', 'assests/baddie.png', 32);
+	game.load.spritesheet('baddie', 'assets/baddie.png', 32);
 }
 
 function create(){
@@ -41,7 +41,7 @@ function create(){
 	player = game.add.sprite(32, 400, 'dude');
 		player.animations.add('left', [0,1,2,3], 10, true);
 		player.animations.add('right', [5,6,7,8], 10, true);
-		game.physic.arcade.enable(player);
+		game.physics.arcade.enable(player);
 		player.body.bounce.y = 0.2;
 		player.body.gravity.y = 300;
 		player.body.colliderWorldBounds = true;
@@ -49,14 +49,29 @@ function create(){
 	enemy1 = game.add.sprite(760, 20, 'baddie');
 		enemy1.animations.add('left', [0,1], 10, true);
 		enemy1.animations.add('right', [2,3], 10, true);
-		game.physic.arcade.enable(enemy1);
+		game.physics.arcade.enable(enemy1);
 		enemy1.body.bounce.y = 0.2;
 		enemy1.body.gravity.y = 300;
 		enemy1.body.colliderWorldBounds = true;
+
+	stars = game.add.physicsGroup();
+	stars.enableBody = true;
+
+	for (var i = 0; i < 12; i++){
+		var star = stars.create(i*70, 0, 'star'); 
+		star.body.gravity.y = 200;
+		star.body.bounce.y = 0.7 + Math.random() * 0.2;
+	}
+	cursors = game.input.keyboard.createCursorKeys();
+
 }
 
 function update(){
+	game.physics.arcade.collide(player, platforms);
+	game.physics.arcade.collide(stars, platforms);
+	game.physics.arcade.collide(enemy1, platforms);
 
+	player.body.velocity.x = 0;
 
 }
 
